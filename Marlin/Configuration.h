e9e1bc48e02a887d3244a0c176f7cb9c60ad35f6
SUF <suf@freemail.hu>
2018-03-05 23:42:22 +0100

    Z Safe Homing enabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1031,11 +1031,11 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing when homing all axes (G28).
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-//#define Z_SAFE_HOMING
+#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2)    // X point for Z homing when homing all axes (G28).
-  #define Z_SAFE_HOMING_Y_POINT ((Y_BED_SIZE) / 2)    // Y point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_X_POINT 14    // X point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_Y_POINT 28    // Y point for Z homing when homing all axes (G28).
 #endif
 
 // Homing speeds (mm/m)
